70d18b4c2ba3ea43ebfe0ac850e704948cd3d3d8
Sachille Atapattu <sa2257@cornell.edu>
2020-04-22 13:19:52 -0400

    Pure embed didn't work

--- tests/example/embed/embed.c
+++ tests/example/embed/embed.c
@@ -1,37 +1,70 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
-  
+
 int main(int argc, char *argv[])
 {
-    int counter; 
-    printf("Program Name Is: %s",argv[0]); 
-    if(argc==1) 
-        printf("\nNo Extra Command Line Argument Passed Other Than Program Name"); 
-        printf("\n");
-    if(argc>=2) 
-    { 
-        printf("\nNumber Of Arguments Passed: %d",argc); 
-        printf("\n----Following Are The Command Line Arguments Passed----"); 
-        for(counter=0;counter<argc;counter++) 
-            printf("\nargv[%d]: %s",counter,argv[counter]); 
-        printf("\n");
+    PyObject *pName, *pModule, *pFunc;
+    PyObject *pArgs, *pValue;
+    int i;
+
+    if (argc < 3) {
+        fprintf(stderr,"Usage: call pythonfile funcname [args]\n");
+        return 1;
     }
- 
-    wchar_t *program = Py_DecodeLocale(argv[1], NULL);
-    if (program == NULL) {
-        fprintf(stderr, "Fatal error: cannot decode argv[1]\n");
-        exit(1);
-    }
-    printf("The program is %s\n", argv[1]);
-    printf("At the start of the program!\n");
-    Py_SetProgramName(program);  /* optional but recommended */
+
     Py_Initialize();
-    PyRun_SimpleString("from time import time,ctime\n"
-                       "print('Today is', ctime(time()))\n");
-    if (Py_FinalizeEx() < 0) {
-        exit(120);
+    pName = PyUnicode_DecodeFSDefault(argv[1]);
+    /* Error checking of pName left out */
+
+    pModule = PyImport_Import(pName);
+    Py_DECREF(pName);
+
+    if (pModule != NULL) {
+        pFunc = PyObject_GetAttrString(pModule, argv[2]);
+        /* pFunc is a new reference */
+
+        if (pFunc && PyCallable_Check(pFunc)) {
+            pArgs = PyTuple_New(argc - 3);
+            for (i = 0; i < argc - 3; ++i) {
+                pValue = PyLong_FromLong(atoi(argv[i + 3]));
+                if (!pValue) {
+                    Py_DECREF(pArgs);
+                    Py_DECREF(pModule);
+                    fprintf(stderr, "Cannot convert argument\n");
+                    return 1;
+                }
+                /* pValue reference stolen here: */
+                PyTuple_SetItem(pArgs, i, pValue);
+            }
+            pValue = PyObject_CallObject(pFunc, pArgs);
+            Py_DECREF(pArgs);
+            if (pValue != NULL) {
+                printf("Result of call: %ld\n", PyLong_AsLong(pValue));
+                Py_DECREF(pValue);
+            }
+            else {
+                Py_DECREF(pFunc);
+                Py_DECREF(pModule);
+                PyErr_Print();
+                fprintf(stderr,"Call failed\n");
+                return 1;
+            }
+        }
+        else {
+            if (PyErr_Occurred())
+                PyErr_Print();
+            fprintf(stderr, "Cannot find function \"%s\"\n", argv[2]);
+        }
+        Py_XDECREF(pFunc);
+        Py_DECREF(pModule);
     }
-    PyMem_RawFree(program);
-    printf("Done with the program!\n");
+    else {
+        PyErr_Print();
+        fprintf(stderr, "Failed to load \"%s\"\n", argv[1]);
+        return 1;
+    }
+    if (Py_FinalizeEx() < 0) {
+        return 120;
+    }
     return 0;
 }
